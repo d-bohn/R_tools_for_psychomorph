4c2ac9428596be86b30ba407aceb7f543506d1a5
Daniel Albohn <dna5021@psu.edu>
2017-02-26 16:42:11 -0500

    added eye volume script

--- R_scripts/tem_to_eyeArea.R
+++ R_scripts/tem_to_eyeArea.R
@@ -1,13 +1,13 @@
 # Take a .tem with 179 or 189 points and convert [x,y]
-# coordinates to fWH ratio.
+# coordinates to eye area.
 
-## FOR DEBUGGING ##
+# # FOR DEBUGGING ##
 # tem <- 'data/3dsk_female_avg_convert.tem'
 # tem <- 'data/MN1.tem'
 # tem <- files[4]
-## END DEBUGGING ##
+# # END DEBUGGING ##
 
-tem_to_fwhr <- function(tem) {
+tem_to_eyeArea <- function(tem) {
   paks <- c('readr','dplyr','tidyr')
   lapply(paks, require, character.only=TRUE)
   
@@ -22,28 +22,39 @@ tem_to_fwhr <- function(tem) {
     data$x <- as.numeric(as.character(data$x))
     data$y <- as.numeric(as.character(data$y))
     
-    fwhr <- with(data,
-                 abs(max(x[114],x[113],x[115])-min(x[112],x[112],x[120]))/abs(y[91]-min(y[21],y[26]))
-    )
+    ##Left eye
+    a <- abs(data$x[1] - data$x[23])
+    b <- abs(data$y[1] - data$y[21])
+    eyeAreaL <- a*b*pi
+    
+    ##Right eye
+    a <- abs(data$x[2] - data$x[24])
+    b <- abs(data$y[2] - data$y[26])
+    eyeAreaR <- a*b*pi
     
   } else if (length(df) == 2) {
     data <- df
     names(data) <- c('x','y')
-    data <- data %>%
-      mutate(., row = seq(0, rows)) %>% 
+    data <- data %>% mutate(., row = seq(0, rows)) %>% 
       filter(., is.na(y) == FALSE)
     data$x <- as.numeric(as.character(data$x))
     data$y <- as.numeric(as.character(data$y))
     
-    fwhr <- with(data,
-                 abs(max(x[114],x[113],x[115])-min(x[112],x[112],x[120]))/abs(y[91]-min(y[21],y[26]))
-    )
+    ##Left eye
+    a <- abs(data$x[1] - data$x[23])
+    b <- abs(data$y[1] - data$y[21])
+    eyeAreaL <- a*b*pi
+    
+    ##Right eye
+    a <- abs(data$x[2] - data$x[24])
+    b <- abs(data$y[2] - data$y[26])
+    eyeAreaR <- a*b*pi
     
   } else {
     print("This template is unrecognized. Do you have the correct formatting?")
+    
+  }
   
-    }
-  
-  return(list(image = tem, fwhr = fwhr))
+  return(list(image = tem, right_eye=eyeAreaR, left_eye=eyeAreaL))
   
 }
--- R_scripts/tem_to_eyespace.R
+++ R_scripts/tem_to_eyespace.R
@@ -7,7 +7,7 @@
 # tem <- files[4]
 ## END DEBUGGING ##
 
-tem_to_fwhr <- function(tem) {
+tem_to_eyespace <- function(tem) {
   paks <- c('readr','dplyr','tidyr')
   lapply(paks, require, character.only=TRUE)
   
@@ -22,28 +22,27 @@ tem_to_fwhr <- function(tem) {
     data$x <- as.numeric(as.character(data$x))
     data$y <- as.numeric(as.character(data$y))
     
-    fwhr <- with(data,
-                 abs(max(x[114],x[113],x[115])-min(x[112],x[112],x[120]))/abs(y[91]-min(y[21],y[26]))
+    eyespace <- with(data,
+                     sqrt(((x[1]-x[2])^2) + ((y[1]-y[2])^2))
     )
     
   } else if (length(df) == 2) {
     data <- df
     names(data) <- c('x','y')
-    data <- data %>%
-      mutate(., row = seq(0, rows)) %>% 
+    data <- data %>% mutate(., row = seq(0, rows)) %>% 
       filter(., is.na(y) == FALSE)
     data$x <- as.numeric(as.character(data$x))
     data$y <- as.numeric(as.character(data$y))
     
-    fwhr <- with(data,
-                 abs(max(x[114],x[113],x[115])-min(x[112],x[112],x[120]))/abs(y[91]-min(y[21],y[26]))
+    eyespace <- with(data,
+                     sqrt(((x[1]-x[2])^2) + ((y[1]-y[2])^2))
     )
     
   } else {
     print("This template is unrecognized. Do you have the correct formatting?")
+    
+  }
   
-    }
-  
-  return(list(image = tem, fwhr = fwhr))
+  return(list(image = tem, eye_spacing = eyespace))
   
 }
